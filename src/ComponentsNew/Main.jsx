68842b7556f15d7c6799ec80759a30060723d048
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2025-01-10 04:32:33 +0530

    gg

--- src/ComponentsNew/Main.jsx
+++ src/ComponentsNew/Main.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import './main.css'
+import memesData from '../memesData'
 
 const Main = () => {
+
+  
+  function randomMeme(){
+    const image = memesData.data.memes
+    const randomIndex = Math.floor(Math.random() * image.length)
+    console.log(randomIndex)
+    const url = image[randomIndex].url
+    console.log(url)
+  }
+
   return (
-    <div>Main</div>
+    <div>
+      <div className='main'>
+        <input placeholder='Top Text'/>
+        <input placeholder='Bottom Text'/>
+      </div>
+
+      <div className='buttonGen'>
+        <button onClick={randomMeme}>Get a new meme image</button>
+      </div>
+    </div>
   )
 }
 
